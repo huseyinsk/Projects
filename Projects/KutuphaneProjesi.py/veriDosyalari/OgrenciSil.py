b06dbdbae743095b92690f90f3864f2e24eda4c7
Huseyin <huseyinsk77@gmail.com>
2024-12-07 02:15:17 +0300

    Update OgrenciSil.py

--- Projects/KutuphaneProjesi.py/veriDosyalari/OgrenciSil.py
+++ Projects/KutuphaneProjesi.py/veriDosyalari/OgrenciSil.py
@@ -3,5 +3,8 @@ from veriDosyalari.hesaplarDB import ogrenci_tcNO as sozluk
 
 def ogrenciSil():
     tc = int(input("Silmek istediğiniz öğrenci kaydına ait tc kimlik no'yu giriniz: "))
-    del sozluk[tc]
-    print("Kayıt başarıyla silindi")
+    if tc in sozluk.keys():
+        del sozluk[tc]
+        print("Kayıt başarıyla silindi")
+    else:
+        print(f"Girmiş olduğunuz {tc} TC Kimlik numarası sistemde kayıtlı değildir.")
